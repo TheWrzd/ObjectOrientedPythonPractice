89a845d5daa19c62a94e088a4b10b331ed64a02d
OsborneTaylorIII <oztay113@gmail.com>
2023-04-03 16:47:14 -0400

    BalloonGame

---
+++ Widgets/main.py
@@ -0,0 +1,15 @@
+# Classes included:
+# TextButton - standard art text strings | CustomButton - Button custom artwork 
+# TextCheckBox - Checkbox with standard art built from a text box
+# TextRadioButton - Radio button with standard art built from text string
+# CustomRadioButton - 
+# DisplayText - Field used to display output text
+# InputText - Field where user can type
+# Dragger - Allows the usre to drag an image 
+# Image - Displays an image at a location
+# ImageCollection - Displays one of a collection of images at a location
+# Animation - Displays a sequence of images
+# SpriteSheetAnimation - Displays a sequence of images from a single larger image
+
+import pygame_widgets
+
